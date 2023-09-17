366ff204a4c311e221027438130f6c6744279dd0
Membathisi Mcetywa <iamemba10gmail.com>
2023-09-18 01:14:51 +0200

    chore(services): removed unused code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,32 +1,8 @@
-// validation-client.service.ts
 import { Injectable } from '@nestjs/common';
-import {
-  ClientProxy,
-  ClientProxyFactory,
-  Transport,
-} from '@nestjs/microservices';
-import { lastValueFrom } from 'rxjs';
 
 @Injectable()
-export class ValidationClientService {
-  private client: ClientProxy;
-
-  constructor() {
-    this.client = ClientProxyFactory.create({
-      transport: Transport.TCP,
-      options: {
-        host: 'localhost',
-        port: 9001, // Port of the validation microservice
-      },
-    });
-  }
-
-  async validatePhoneNumber(phoneNumbers: string[]) {
-    // Send a validation request to the validation microservice
-    const client = this.client.send<string[], string[]>(
-      'validate-phone',
-      phoneNumbers,
-    );
-    return lastValueFrom(client);
+export class AppService {
+  getHello(): string {
+    return 'Hello, World!';
   }
 }
