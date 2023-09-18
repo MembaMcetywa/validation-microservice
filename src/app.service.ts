cf4842964c0af32c95e251a543a0168b79eb6fc8
Membathisi Mcetywa <iamemba10gmail.com>
2023-09-18 03:50:48 +0200

    feature(services): added logic to save phone number

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { PhoneNumber, PhoneNumberDocument } from './phone-number-schema';
+import { PhoneNumberDTO } from './phone-number-dto';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectModel(PhoneNumber.name)
+    private readonly phoneNumberModel: Model<PhoneNumberDocument>,
+  ) {}
+
+  async savePhoneNumber(phoneData: PhoneNumberDTO): Promise<PhoneNumber> {
+    const newPhoneNumber = new this.phoneNumberModel(phoneData);
+    return await newPhoneNumber.save();
+  }
   getHello(): string {
     return 'Hello, World!';
   }
