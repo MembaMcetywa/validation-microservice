f3f351f79441832f168dee83b6996dd6c4e11e9a
Membathisi Mcetywa <iamemba10gmail.com>
2023-09-18 02:11:19 +0200

    feature(validation-microservice): added phone number dto

---
+++ src/phone-number-dto.ts
@@ -0,0 +1,7 @@
+export class PhoneNumber {
+  phoneNumber: string;
+  phoneNumberCountryCode: string;
+  phoneNumberType: string;
+  phoneNumberIsValid: boolean;
+  phoneNumberIsPossible: boolean;
+}
